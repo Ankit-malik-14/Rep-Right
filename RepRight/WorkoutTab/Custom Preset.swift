da43c4bd88d6729184f1cff6d3b653ca5067176a
Mayurakshi Das <mayurakshikarate@gmail.com>
2026-03-13 09:43:08 +0530

    trying

---
+++ RepRight/WorkoutTab/Custom Preset.swift
@@ -0,0 +1,21 @@
+//
+//  Custom Preset.swift
+//  RepRight
+//
+//  Created by Mayurakshi Das on 13/03/26.
+//
+
+import SwiftUI
+
+struct Custom_Preset: View {
+    var body: some View {
+        VStack{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Hii")
+        }
+    }
+}
+
+#Preview {
+    Custom_Preset()
+}
