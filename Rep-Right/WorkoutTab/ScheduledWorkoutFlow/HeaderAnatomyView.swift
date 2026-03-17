7338b3a0a016b0c64d66e99eeea03ffd1e40209f
Abhyansh22 <abhyansh22@gmail.com>
2026-03-18 01:29:17 +0530

    Added Scheduled workout flow(starts after tapping on "Strat Workout" on scheduled preset), WorkoutDetailView, WarmUpExerciseView with their respective sub-views.

---
+++ Rep-Right/WorkoutTab/ScheduledWorkoutFlow/HeaderAnatomyView.swift
@@ -0,0 +1,27 @@
+//
+//  HeaderAnatomyView.swift
+//  PresettFlow
+//
+//  Created by Jugad on 18/03/26.
+//
+
+import SwiftUI
+
+struct HeaderAnatomyView: View {
+    var body: some View {
+        ZStack {
+            Color(UIColor.systemGray6)
+            
+            // Placeholder for the muscular back image
+            Image(systemName: "figure.mind.and.body")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.gray)
+                .frame(width: 180)
+                .padding(.top, 20)
+                .padding(.bottom, 60)
+        }
+        .frame(height: 260)
+        .cornerRadius(20)
+    }
+}
