1527267e6890bc290e6d260c50ba21209fbdb886
Mayurakshi Das <mayurakshikarate@gmail.com>
2026-03-17 11:27:19 +0530

    trying to make onboarding screen

--- Rep-Right/OnboardingScreens/OnboardingScreen1.swift
+++ Rep-Right/OnboardingScreens/OnboardingScreen1.swift
@@ -7,13 +7,52 @@
 
 import SwiftUI
 
-struct OnboardingScreen1: View {
+struct AllOnboardingScreens: View {
     var body: some View {
+        TabView{
+            Tab(){
+                OnboardingScreen1()
+            }
+            Tab(){
+                OnboardingScreen1()
+            }
+        }.tabViewStyle(.page)
+    }
+}
+
+
+struct OnboardingScreen1: View {
+    @Environment(\.dismiss) private var dismiss
+    var body: some View {
+        NavigationStack{
+            VStack{
+                Image(systemName: "dumbbell.fill")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                VStack{
+                    Text("Your Personal AI Fitness Assistance")
+                        .font(.largeTitle)
+                        .bold()
+                    //                    .padding()
+                    Text("Real-time posture assistance and workout plans to help you reach your goals safely")
+                        .font(.headline)
+                        .foregroundStyle(.secondary)
+                        .padding()
+                }
+            }
+        }
+    }
+}
+
+struct firstEmptyView: View{
+    @State private var showSheet: Bool = true
+    var body: some View{
         VStack{
-            Image(systemName: "dumbbell.fill")
-                .font(.largeTitle)
-                .bold()
-            
+            Text("hii")
+        }
+        .sheet(isPresented: $showSheet){
+            OnboardingScreen1()
         }
     }
 }
