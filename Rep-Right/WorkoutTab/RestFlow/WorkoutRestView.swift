51e519f819d45f401eac5a97d91476ed898b1d67
Abhyansh22 <abhyansh22@gmail.com>
2026-03-22 02:54:55 +0530

    Rest card implementation, -> WorkoutRestView dummy page to start rest sheet view with basic state variables -> RestCardVuew cotains main rest timer view with basic bindings -> NextExerciseCardView an optional card view that appears if the current set is last set of the ongoing exercise.

---
+++ Rep-Right/WorkoutTab/RestFlow/WorkoutRestView.swift
@@ -0,0 +1,40 @@
+//
+//  WorkoutRestView.swift
+//  Rep-Right
+//
+//  Created by GU on 19/03/26.
+//
+
+import SwiftUI
+
+struct WorkoutRestView: View {
+    
+    @State var isSetFinished: Bool = false
+    
+    var body: some View {
+        ZStack{
+            Color.blue
+                .ignoresSafeArea()
+            //Text("Will be acting as sheet View for rest day")
+            Button {
+                isSetFinished.toggle()
+            } label: {
+                Label("Finish Set", systemImage: "checkmark")
+                    .font(.title3.bold())
+            }
+            .foregroundStyle(.orange)
+            .padding()
+            .background(.gray.opacity(0.4), in: RoundedRectangle(cornerRadius: 8))
+            .sheet(isPresented: $isSetFinished){
+                RestCardView(dismissSheet: $isSetFinished, isLastSet: false)
+                    .presentationDetents([.medium])
+            }
+            
+        }
+        
+    }
+}
+
+#Preview {
+    WorkoutRestView()
+}
