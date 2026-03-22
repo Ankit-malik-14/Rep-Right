8dd679b1b7499941ca634c01b866099a4878d012
Mayurakshi Das <mayurakshikarate@gmail.com>
2026-03-22 22:51:32 +0530

    Made caliberation Screens. currently it is hardcoded.

--- Rep-Right/WorkoutTab/HomeScreenElements/PresetsAccordingToBodyParts.swift
+++ Rep-Right/WorkoutTab/HomeScreenElements/PresetsAccordingToBodyParts.swift
@@ -43,7 +43,7 @@ struct PresetsAccordingToBodyParts: View {
 }
 
 #Preview {
-    @Previewable @Environment(Presets.self) var preset
+//    @Previewable @Environment(Presets.self) var preset
     PresetsAccordingToBodyParts()
         .environment(Presets())
 }
