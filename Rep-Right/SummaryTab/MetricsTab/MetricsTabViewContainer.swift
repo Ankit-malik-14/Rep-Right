720e01947cbb9a6fd2597bad48b9e39688dc9db3
Manas Dubey <147379602+vikingmanas@users.noreply.github.com>
2026-03-23 14:21:38 +0530

    Added summary metric view
    here i have added
    Ring for exercise
    Graph for time and exercise u did

---
+++ Rep-Right/SummaryTab/MetricsTab/MetricsTabViewContainer.swift
@@ -0,0 +1,27 @@
+//
+//  MetricsTabViewContainer.swift
+//  Rep-Right
+//
+//  Created by GU on 23/03/26.
+//
+
+import SwiftUI
+
+struct MetricsTabViewContainer: View {
+    var body: some View {
+    ScrollView{
+            VStack(spacing: 20){
+                ExerciseRingView()
+                Spacer()
+            totalTimeExerciseView()
+            }.navigationTitle("Metrics")
+    }
+    }
+}
+
+#Preview {
+    NavigationStack{
+        MetricsTabViewContainer()
+    }
+}
+
