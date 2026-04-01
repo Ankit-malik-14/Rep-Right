27212fe971ad48a7ba6a8720b4d7ad3caf926932
Mayurakshi Das <mayurakshikarate@gmail.com>
2026-04-01 19:28:48 +0530

    made data intake view for users.

---
+++ Rep-Right/DataIntake/UserCalorieIntake.swift
@@ -0,0 +1,18 @@
+//
+//  UserCalorieIntake.swift
+//  Rep_Right
+//
+//  Created by Mayurakshi Das on 01/04/26.
+//
+
+import SwiftUI
+
+struct UserCalorieIntake: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserCalorieIntake()
+}
---
+++ Rep-Right/DataIntake/UserDataIntake.swift
@@ -0,0 +1,18 @@
+//
+//  UserDataIntake.swift
+//  Rep_Right
+//
+//  Created by Mayurakshi Das on 01/04/26.
+//
+
+import SwiftUI
+
+struct UserDataIntake: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDataIntake()
+}
