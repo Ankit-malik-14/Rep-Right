003a37aa60386ce02660c32727df11457d63a548
Abhyansh22 <abhyansh22@gmail.com>
2026-04-06 02:48:09 +0530

    Recommendation system plan

--- Rep-Right/RecommendationSystem/RecommendationEngine.swift
+++ Rep-Right/RecommendationSystem/RecommendationEngine.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
+//a recommendation engine to suggest preset, with a enum targetMuscleGroup containig cases back, shoulder, chest, core, arms, legs as general rule of thumb after training a targetMuscleGroup atleast 48-72hr should be given for recovery so using the daily summary of previous 7 days, first layer will be to remove targetMuscleGroup trained under 48-72hr then will recommend preset according to LRU type algo.
+
 //@Environment(DummyWorkoutSummaryData) var recentActivity
 // from the logs of previous two weeks we will get the data about what targetMuscleGroup has been trained to suggest next preset of targetMuscleGroup that doesn't need 48-72hr recovery.
-// if for someday user has only done standalone exercises then it is hard to guess targetMuscleGroup of the exercises because exercises feature smaller segments an exercise trains.
-//a recommendation engine to suggest preset, with a enum targetMuscleGroup containig cases back, shoulder, chest, core, arms, legs as general rule of thumb after training a targetMuscleGroup atleast 48-72hr should be given for recovery so I will use the daily summary of previous 7 days, first layer will be to remove targetMuscleGroup trained under 48-72hr then will suggest the according to LRU
+// if for someday user has only done standalone exercises then it is hard to guess targetMuscleGroup of the exercises because exercises feature smaller segments an exercise trains. To solve this specific problem we can try to map these smaller segments to their relative targetMuscleGroup.
+
